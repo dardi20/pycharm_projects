f9dd938435d8e3c1e0da2c96ee508912bc1e4210
Dardi Rrapaj <dardi259@gmail.com>
2020-07-14 17:21:43 +0200

    u shtua nje koment i ri

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from Student import Student
 
 #nje koment i ri
 person = Person("Dardi", "Rrapaj")
-
+#dhe nje coment tjt plus
+# nje gj tjt palidhje ketu
 print(person.get_fullname())
 
 student = Student("Eralda", "Yzeiraj")
